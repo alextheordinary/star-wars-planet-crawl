04b51591475cfce069bb3b9c4988858f1e7ef26b
Alex Bellamy <alexjbellamy@gmail.com>
2022-08-15 10:11:54 -0400

    added test functions to call Bored API and SWAPI. These functions log the data to console

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Initial js file for the project
+
+// Test function for calling the Bored API
+function testBored() {
+    var queryURL = "http://www.boredapi.com/api/activity/";
+
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
+
+
+// Test function for calling the Star Wars API (SWAPI)
+function testSWAPI() {
+    var queryURL = "https://swapi.dev/api/";
+
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
+
+testBored();
+testSWAPI();
